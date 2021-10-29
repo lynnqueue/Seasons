54cf85f62ad3bfe2ffa114e7a3d90d1c6cb92469
Lynn_Qiu <llynnqiu@gmail.com>
2021-10-29 16:37:19 -0400

    Adding season-indicating title

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+}
+
+const SeasonDisplay = ({ lat }) => {
+  const season = getSeason(lat, new Date().getMonth());
+  const text = season === 'summer' ? 'Lets hit the beach!' : 'Burr, its chilly!';
+
+  return (
+       <div>
+         <h1>{text}</h1>
+       </div> 
+  ); 
 };
 
 export default SeasonDisplay;
